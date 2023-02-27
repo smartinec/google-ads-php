d1c6b9ed1a59eaf5ee42aa863a1dbfb1430aecbe
Shaun Martinec <smartinec@gmail.com>
2023-02-27 11:52:27 -0500

    PHP 7.2 Compatibility for V13

--- src/Google/Ads/GoogleAds/Lib/V13/ServiceClientFactoryTrait.php
+++ src/Google/Ads/GoogleAds/Lib/V13/ServiceClientFactoryTrait.php
@@ -202,9 +202,9 @@ trait ServiceClientFactoryTrait
                 $googleAdsLoggingInterceptor
             );
         }
-        array_push(
+        $clientOptions['transportConfig']['grpc']['interceptors'] = array_merge(
             $clientOptions['transportConfig']['grpc']['interceptors'],
-            ...$this->getGrpcInterceptors()
+            $this->getGrpcInterceptors()
         );
         if (!empty($this->getProxy())) {
             putenv('http_proxy=' . $this->getProxy());
